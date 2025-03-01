b84e66309afa40bacaabca43e1016a512c0b8e00
betabites <BetaBit3s@gmail.com>
2025-03-01 22:45:26 +1300

    Add position and transformation options for ReactBot
    Introduced new configurations to control ReactBot's position (corner selection) and transformations (horizontal and vertical flipping). Updated styles, logic, and UI to support these features, along with an idle animation setup for smoother transitions.

--- options.js
+++ options.js
@@ -10,22 +10,30 @@ function load() {
     const autoPause = document.getElementById("auto-pause");
     const autoReaction = document.getElementById("auto-reaction");
     const scale = document.getElementById("scale");
+    const corner = document.getElementById("corner");
+    const horizontal = document.getElementById("horizontal");
+    const vertical = document.getElementById("vertical");
     if (!intervalMin || !intervalMax || !autoPause)
         throw new Error(
             "Could not find intervalMin, intervalMax or autoPause"
         )
 
-    let data = {intervalMin: 30, intervalMax: 300, autoPause: false, scale: 50}
-    getBrowserAPI().storage.sync.get(["intervalMin", "intervalMax", "autoPause", "autoReaction", "scale"]).then((values) => {
+    let data = {intervalMin: 30, intervalMax: 300, autoPause: false, scale: 50, horizontal: false, vertical: false, corner: "tl"};
+    getBrowserAPI().storage.sync.get(["intervalMin", "intervalMax", "autoPause", "autoReaction", "scale", "corner", "horizontal", "vertical"]).then((values) => {
         intervalMin.value = values.intervalMin ?? 30;
         intervalMax.value = values.intervalMax ?? 300;
-        autoPause.checked = values.autoPause ?? false;
+        autoPause.checked = values.autoPause ?? true;
         autoReaction.checked = values.autoReaction ?? true;
         scale.value = values.scale ?? 50;
+        corner.value = values.corner ?? "tl";
+        horizontal.checked = values.horizontal ?? false;
+        vertical.checked = values.vertical ?? false;
+        console.log("LOADED", values)
         data = values;
     })
 
     function save() {
+        console.log("SAVING", data)
         getBrowserAPI().storage.sync.set(data)
     }
 
@@ -43,6 +51,19 @@ function load() {
     }
     autoReaction.onchange = () => {
         data.autoReaction = autoReaction.checked;
+        save();
+    }
+    corner.onchange = () => {
+        data.corner = corner.value;
+        save();
+    }
+    horizontal.onchange = () => {
+        console.log("HORIZONTAL CHANGED")
+        data.horizontal = horizontal.checked;
+        save();
+    }
+    vertical.onchange = () => {
+        data.vertical = vertical.checked;
         save();
     }
     scale.onpointermove = () => {
